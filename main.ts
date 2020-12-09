94215638e005d28be916b5fd4797d092ad79e277
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 19:36:10 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let die = 0
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    die = randint(1, 6) + randint(1, 6)
+    die = randint(1, 6) + (randint(1, 6) + randint(1, 6))
     counter = 0
-    for (let y = 0; y <= 2; y++) {
+    for (let y = 0; y <= 3; y++) {
         for (let x = 0; x <= 4; x++) {
             if (counter < die) {
                 led.plot(x, y)
