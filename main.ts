ae4ab215165215081144a483cb78083c71bb1d29
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 19:09:46 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let die = 0
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    die = randint(1, 6)
+    counter = 0
+    for (let y = 0; y <= 1; y++) {
+        for (let x = 0; x <= 4; x++) {
+            if (counter < die) {
+                led.plot(x, y)
+            } else {
+                led.unplot(x, y)
+            }
+            basic.pause(50)
+            counter += 1
+        }
+    }
+})
 basic.forever(function () {
 	
 })
